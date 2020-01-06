95325ca70dd36d54e859a872782f0c1806142830
Dragos Dumitrache <dragosd2000@gmail.com>
2020-01-06 17:04:16 +0000

    Working on rust mode

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -2,6 +2,10 @@
 ;;; Commentary:
 ;;; Code:
 
+;;; init-rust.el --- Support for the Rust language -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
 (when (maybe-require-package 'rust-mode)
   (when (maybe-require-package 'racer)
     (add-hook 'rust-mode-hook #'racer-mode))
@@ -12,79 +16,91 @@
   (after-load 'rust-mode
     (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
 
-;; (use-package flycheck
-;;   :hook (prog-mode . flycheck-mode))
+(use-package racer
+  :requires rust-mode
 
-;; (use-package company
-;;   :hook (prog-mode . company-mode)
-;;   :config (setq company-tooltip-align-annotations t)
-;;   (setq company-minimum-prefix-length 1))
+  :init (setq racer-rust-src-path
+              (concat (string-trim
+                       (shell-command-to-string "rustc --print sysroot"))
+                      "/lib/rustlib/src/rust/src"))
 
-;; (use-package lsp-mode
-;;   :commands lsp
-;;   :config (require 'lsp-clients)
-;;   :ensure t)
+  :config
+  (add-hook 'rust-mode-hook #'racer-mode)
+  (add-hook 'racer-mode-hook #'eldoc-mode)
+  (add-hook 'racer-mode-hook #'company-mode))
 
-;; (use-package cargo
-;;   :ensure t
-;;   :diminish cargo-minor-mode
-;;   :hook (rust-mode . cargo-minor-mode))
+(use-package toml-mode)
 
-;; (use-package toml-mode
-;;   :ensure t)
+(use-package rust-mode
+  :hook (rust-mode . lsp))
 
-;; (use-package lsp-ui
-;;   :ensure t)
+(require-package 'rust-mode)
+;;(use-package dap-mode
+;;  :requires (rust-mode)
+;;  (add-hook 'rust-mode-hook #'dap-mode))
 
-;; (use-package rust-mode
-;;   :hook (rust-mode . lsp))
-
-;; ;; Add keybindings for interacting with Cargo
-;; (use-package cargo
-;;   :hook (rust-mode . cargo-minor-mode))
-
-;; (use-package racer
-;;   :hook (rust-mode . racer-mode))
-;; (use-package flycheck-rust
-;;   :config (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
-
-;; (when (maybe-require-package 'rust-mode)
-;;   (when (maybe-require-package 'racer)
-;;     (add-hook 'rust-mode-hook #'racer-mode))
-;;   (when (maybe-require-package 'company)
-;;     (add-hook 'racer-mode-hook #'company-mode)))
-
-;; (when (maybe-require-package 'flycheck-rust)
-;;   (after-load 'rust-mode
-;;     (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
-
-;; ;; ;; (use-package rustic
-;; ;;   :ensure t)
-;; (setq rust-format-on-save t)
-;; (global-set-key (kbd "s-b") 'lsp-find-definition)
-;; ;; (add-hook 'rust-mode-hook #'racer-mode)
-;; ;; (add-hook 'racer-mode-hook #'eldoc-mode)
-;; (add-hook 'racer-mode-hook #'company-mode)
-
-;; (require 'rust-mode)
-;; (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+;; ;; Map TAB key to completions.
+;; (local-set-key (kbd "TAB") 'company-indent-or-complete-common)
 ;; (setq company-tooltip-align-annotations t)
 
-(use-package lsp-mode
-  :hook (rust-mode . lsp)
-  :commands lsp)
+;; (use-package company :ensure t
+;;   :config
+;;   (setq company-idle-delay 0.2)
+;;   (add-hook 'racer-mode-hook
+;;     (lambda ()
+;;       (setq-local company-tooltip-align-annotations t))))
 
-;; optionally
-(use-package lsp-ui :commands lsp-ui-mode)
-(use-package company-lsp :commands company-lsp)
-(use-package helm-lsp :commands helm-lsp-workspace-symbol)
-(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
-;; optionally if you want to use debugger
-(use-package dap-mode
-  :ensure t)
 
-(use-package dap-rust
-  :ensure t)
+
+;; ;; Flycheck-Begin. On the fly syntax checking for multiple programming languages.
+;; ;; Enable Flycheck in buffers with supported languages.
+;; (use-package flycheck :ensure t)
+;; (use-package flycheck-rust :ensure t)
+;; (use-package rust-mode :ensure t
+;;   :config
+;;   (setq rust-format-on-save t))
+
+;; (use-package racer :ensure t
+;;   :config
+;;   (progn
+;;         (add-hook 'rust-mode-hook #'racer-mode) ;; Activate racer in rust buffers.
+;;         (add-hook 'racer-mode-hook #'eldoc-mode) ;; Shows signature of current function in minibuffer.
+;;         ;; Rust completions with Company and Racer.
+;;         (add-hook 'racer-mode-hook #'company-mode)))
+
+;; (use-package company :ensure t
+;;   :config
+;;   (setq company-idle-delay 0.2)
+;;   (add-hook 'racer-mode-hook
+;;     (lambda ()
+;;       (setq-local company-tooltip-align-annotations t))))
+
+;; ;; Enable Cargo minor mode allows us to do cargo commands
+;; ;; rust-mode and toml-mode
+;; (use-package cargo :ensure t
+;;   :config
+;;   (progn
+;;   (add-hook 'rust-mode-hook 'cargo-minor-mode)
+;;         (add-hook 'toml-mode-hook 'cargo-minor-mode)))
+
+;; (add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; ;; Flycheck Rust support.
+;; (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+;; ;; Flycheck-End.
+
+;; ;; Path to rust source.
+;; (when (equal system-type 'gnu/linux)
+;;   (setq racer-rust-src-path (concat (getenv "HOME") "/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src")))
+;; (when (equal system-type 'darwin)
+;;   (setq racer-rust-src-path (concat (getenv "HOME") "/.rustup/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/src")))
+;; ;; Racer bin path.
+;; (setq racer-cmd (concat (getenv "HOME") "/.cargo/bin/racer"))
+
+;; ;; Map TAB key to completions.
+;; (local-set-key (kbd "TAB") 'company-indent-or-complete-common)
+;; (setq company-tooltip-align-annotations t)
+
 
 (provide 'init-rust)
 ;;; init-rust.el ends here
